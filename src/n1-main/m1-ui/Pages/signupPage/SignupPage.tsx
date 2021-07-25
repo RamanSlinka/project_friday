06d0af61a34234e47edcfd942551aad3ee0e90d5
RamanSlinka <romuch1986@gmail.com>
2021-07-25 20:52:02 +0300

    reworked the project again :(

--- src/n1-main/m1-ui/Pages/signupPage/SignupPage.tsx
+++ src/n1-main/m1-ui/Pages/signupPage/SignupPage.tsx
@@ -1,51 +1,84 @@
 import React from 'react'
+import {makeStyles, Theme, createStyles} from '@material-ui/core/styles';
 import {FormControl, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
-import InputAdornments from "./PasswordField";
 import s from './signupPage.module.css'
+import {useFormik} from "formik";
+
+
+
+
 
 const SignupPage: React.FC = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            cfPassword: ''
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+        },
+    })
+
+
+
     return (
         <div className={s.container}>
             <div className={s.wrapper}>
                 <Grid container justify="center">
                     <Grid item xs={3}>
-                        <FormControl>
-                            <FormLabel>
-                                <h1>It-incubator</h1>
-                                <h2>Sign up </h2>
+                        <form onSubmit={formik.handleSubmit}>
+                            <FormControl>
+                                <FormLabel>
+                                    <h1>It-incubator</h1>
+                                    <h2>Sign up </h2>
 
-                            </FormLabel>
-                            <FormGroup>
-                                <TextField
-                                    type="email"
-                                    label="Email"
-                                    margin="normal"
-                                />
-
-                                <InputAdornments title={'Password'}/>
-
-                                <InputAdornments title={'Confirm password'}/>
+                                </FormLabel>
+                                <FormGroup>
+                                        <TextField
+                                            type="email"
+                                            label="Email"
+                                            margin="normal"
 
 
-                                <FormGroup style={{display: 'flex', flexDirection: 'row', marginTop: '40px'}}>
-                                    <Button
-                                        style={{margin: '15px'}}
-                                        type={'reset'}
-                                        variant={'outlined'}
-                                        color={'secondary'}
-                                        size={'small'}>Cancel</Button>
+                                            {...formik.getFieldProps('email')}
+                                        />
+                                    <TextField
+                                        type="password"
+                                        label="Password"
+                                        margin="normal"
 
-                                    <Button
-                                        style={{margin: '15px'}}
-                                        type={'submit'}
-                                        variant={'contained'}
-                                        color={'primary'}
-                                        size={'small'}>Register</Button>
 
+                                        {...formik.getFieldProps('password')}
+                                    />
+                                    <TextField
+                                        type="password"
+                                        label="Confirm password"
+                                        margin="normal"
+
+
+                                        {...formik.getFieldProps('cfPassword')}
+                                    />
+
+                                    <FormGroup style={{display: 'flex', flexDirection: 'row', marginTop: '40px'}}>
+                                        <Button
+                                            style={{margin: '15px'}}
+                                            type={'reset'}
+                                            variant={'outlined'}
+                                            color={'secondary'}
+                                            size={'small'}>Cancel</Button>
+
+                                        <Button
+                                            style={{margin: '15px'}}
+                                            type={'submit'}
+                                            variant={'contained'}
+                                            color={'primary'}
+                                            size={'small'}>Register</Button>
+
+                                    </FormGroup>
                                 </FormGroup>
-
-                            </FormGroup>
-                        </FormControl>
+                            </FormControl>
+                        </form>
                     </Grid>
                 </Grid>
 
@@ -53,4 +86,5 @@ const SignupPage: React.FC = () => {
         </div>
     )
 }
+
 export default SignupPage
