168e26be25bd7d6231a6fbf34401b6329412ce66
RamanSlinka <romuch1986@gmail.com>
2021-08-07 22:13:51 +0300

    add Packs and RangeShow components

---
+++ src/n1-main/m1-ui/Pages/Pack/Packs.tsx
@@ -0,0 +1,140 @@
+import React, {useEffect} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import RangeShowCard from './RangeShowCard';
+import PackList from './PacksList'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../../../m2-bll/redux/store";
+
+import {Redirect} from "react-router-dom";
+import {PATH} from "../../Routes";
+import {getAllPack} from "../../../m2-bll/redux/pack-reducer";
+import {GetPackQueryParamsType, UserProfileType} from "../../../m3-dal/Api";
+import {Card, Paper} from "@material-ui/core";
+import DeckTable from "../../common/c8-Table/DeckTable";
+import Search from './Search';
+import Typography from "@material-ui/core/Typography";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        /*flexGrow: 1,*/
+        marginTop: "40px",
+    },
+    paper: {
+        margin: 'auto',
+        maxWidth: 1200,
+    },
+    filterBlock: {
+        backgroundColor: "#ECECF9",
+        maxWidth: "250px",
+        padding: "30px",
+        flexGrow: 1,
+    },
+    mainBlock: {
+        width: "fit-content",
+        flexGrow: 3,
+        margin: "30px",
+        maxWidth: "950px",
+        boxSizing : "border-box",
+    },
+    mainSearchBlock: {
+        /*flexGrow: 1,*/
+        marginBottom: "20px",
+    },
+    mainSearchHeader : {
+        marginBottom: "20px",
+    },
+    mainTableBlock: {
+        /*maxWidth: "500px",*/
+    }
+}));
+
+export default function Pack() {
+    const classes = useStyles();
+
+    const {_id} = useSelector((state: AppStoreType) => state.auth);
+    const pack = useSelector((state: AppStoreType) => state.pack);
+    const dispatch = useDispatch();
+
+    const getAllPacks = () => {
+        let sortPacks
+        if (pack.sortField) {
+            sortPacks = +pack.isSortTypeAscending + pack.sortField;
+        }
+
+        const paramsObject: GetPackQueryParamsType = {
+            params: {
+                ...(pack.packName && {packName: pack.packName}),
+                ...(pack.min && {min: pack.min}),
+                ...(pack.max && {max: pack.max}),
+                ...(pack.page && {page: pack.page}),
+                ...(pack.pageCount && {pageCount: pack.pageCount}),
+                ...(pack.user_id && {user_id: pack.user_id}),
+                ...(sortPacks && {sortPacks: sortPacks}),
+            }
+        }
+        dispatch(getAllPack(paramsObject))
+    };
+
+    useEffect(() => {
+        getAllPacks();
+    }, []);
+
+
+    /*if (!_id) {
+        return (
+            <Redirect from={PATH.ALL_ROUTES} to="/login"/>
+        )
+    }*/
+
+
+    return (
+        <div className={classes.root}>
+            <Paper
+                elevation={4}
+                className={classes.paper}
+                square={false}
+            >
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="stretch"
+                >
+                    <Grid
+                        item
+                        className={classes.filterBlock}
+                    >
+                        <RangeShowCard/>
+                    </Grid>
+                    <Grid
+                        item
+                        className={classes.mainBlock}
+                    >
+                        <Grid
+                            item
+                            className={classes.mainSearchBlock}
+                            alignItems={"stretch"}
+                        >
+                            <Typography
+                                variant={"h2"}
+                                className={classes.mainSearchHeader}
+                            >
+                                Packs list
+                            </Typography>
+                            <Search/>
+                        </Grid>
+                        <Grid
+                            item
+                            className={classes.mainTableBlock}
+                            alignItems={"stretch"}
+                        >
+                            <DeckTable/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </div>
+    );
+}
---
+++ src/n1-main/m1-ui/Pages/PacksListPage.tsx
@@ -0,0 +1,168 @@
+import React, {useEffect, useState} from 'react'
+import {Button, Card, createStyles, Grid, makeStyles, Theme, Typography} from "@material-ui/core";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../../m2-bll/redux/store";
+import {GetPackQueryParamsType, NewPackObjectDataType, PackUpdateObjectType} from "../../m3-dal/Api";
+import {
+    addNewPackTC,
+    deletePackByIdTC,
+    getAllPack,
+    setCardsPackAC, setCardsPackTotalCountAC,
+    setMaxCardsCountAC,
+    setMinCardsCountAC,
+    setPackNameAC, setPackSortType, setPageAC,
+    setPageCountAC,
+    setUserIdAC, updateCardPack
+} from "../../m2-bll/redux/pack-reducer";
+import Table from "../common/c8-Table/Table";
+import DeckTable from "../common/c8-Table/DeckTable";
+
+const useStyles = makeStyles<Theme>(theme => createStyles({
+    root: {
+        textAlign: "center",
+        padding: "30px 30px",
+        /*maxWidth: "413px",*/
+    },
+
+}))
+
+export type PackListPagePropsType = {
+    message?: string
+}
+
+const PacksListPage: React.FC<PackListPagePropsType> = (props) => {
+
+    const classes = useStyles()
+    const {message} = props
+    const dispatch = useDispatch()
+    const pack = useSelector((state: AppStoreType) => state.pack)
+    console.log('Render pack list page', pack)
+    // new Pack Object Fields
+    const [type, setType] = useState<string>('')
+    const [name, setName] = useState<string>('first deck updated')
+    const [path, setPath] = useState<string>('')
+    const [grade, setGrade] = useState<number>(0)
+    const [shots, setShots] = useState<number>(0)
+    const [rating, setRating] = useState<number>(0)
+    const [deckCover, setDeckCover] = useState<string>('')
+    const [privateDeck, setPrivateDeck] = useState<boolean>(false)
+
+    /*type? : string
+    name? : string
+    path? : string
+    grade? : number
+    shots? : number
+    rating? : number
+    deckCover? : string
+    private? : boolean*/
+
+    const getAllPacks = () => {
+        let sortPacks
+        // if sortField set create sortPacks fiedl '0created' '1updated'
+        if (pack.sortField) {
+            sortPacks = +pack.isSortTypeAscending + pack.sortField;
+        }
+
+        const paramsObject: GetPackQueryParamsType = {
+            params: {
+                ...(pack.packName === null && {packName: pack.packName}),
+                ...(pack.min === null && {min: pack.min}),
+                ...(pack.max === null && {max: pack.max}),
+                ...(pack.page === null && {page: pack.page}),
+                ...(pack.pageCount === null && {pageCount: pack.pageCount}),
+                ...(pack.user_id === null && {user_id: pack.user_id}),
+                ...(sortPacks && {sortPacks: sortPacks}),
+            }
+        }
+        dispatch(getAllPack(paramsObject))
+    }
+
+    const deletePackById = () => {
+        let id = "610a4f0684e42f00045c32e5";
+
+        dispatch(deletePackByIdTC(id))
+    }
+
+    const updatePack = () => {
+        let _id = '610a53b49906843f45c0f280';
+        let updatePackObject: PackUpdateObjectType = {
+            cardsPack: {
+                _id : _id,
+                ...(name && {name: name}),
+                ...(path && {path: path}),
+                ...(grade && {grade: grade}),
+                ...(shots && {shots: shots}),
+                ...(rating && {rating: rating}),
+                ...(deckCover && {deckCover: deckCover}),
+                /*...( privateDeck && { private: privateDeck}),*/
+                ...(type && {typeDeck: type}),
+            }
+        }
+        dispatch(updateCardPack(updatePackObject))
+    }
+
+
+    const addPack = () => {
+        let newPackObject: NewPackObjectDataType = {
+            cardsPack: {
+                ...(name && {name: name}),
+                ...(path && {path: path}),
+                ...(grade && {grade: grade}),
+                ...(shots && {shots: shots}),
+                ...(rating && {rating: rating}),
+                ...(deckCover && {deckCover: deckCover}),
+                /*...( privateDeck && { private: privateDeck}),*/
+                ...(type && {typeDeck: type}),
+            }
+        }
+        dispatch(addNewPackTC(newPackObject))
+    }
+
+    useEffect(() => {
+        console.log('Pack reducer state', pack)
+
+    }, [])
+
+    return (
+        <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            style={{minHeight: '100vh'}}
+        >
+            <Card
+                className={classes.root}
+            >
+                <Grid item>
+                    {/*<Button
+                        type={'button'}
+                        variant={'contained'}
+                        onClick={getAllPacks}
+                        color={'primary'}>GetPacks</Button>*/}
+                    <Button
+                        type={'button'}
+                        variant={'contained'}
+                        onClick={deletePackById}
+                        color={'primary'}>Delete pack</Button>
+                    <Button
+                        type={'button'}
+                        variant={'contained'}
+                        onClick={addPack}
+                        color={'primary'}>Add pack</Button>
+                    <Button
+                        type={'button'}
+                        variant={'contained'}
+                        onClick={updatePack}
+                        color={'primary'}>Update pack</Button>
+                    {/*<Table/>*/}
+                </Grid>
+                <Grid item>
+                    <DeckTable/>
+                </Grid>
+            </Card>
+
+        </Grid>
+    )
+}
+
+export default PacksListPage
